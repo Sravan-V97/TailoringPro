d561ea5072d51749df5abcdb4dcb30e30804c5bc
RajeebAK <rajeeb@riafy.me>
2020-12-13 22:24:29 +0530

    fix issue of prodcut card

--- src/views/product/ProductListView/ProductCard.js
+++ src/views/product/ProductListView/ProductCard.js
@@ -31,65 +31,50 @@ const useStyles = makeStyles(theme => ({
 
 const ProductCard = ({ className, product, ...rest }) => {
   const classes = useStyles();
-
-  var [courseObjects, setCourseObjects] = useState({});
-
-  useEffect(() => {
-    firebaseDb.child('coursedetails').on('value', snapshot => {
-      if (snapshot.val() != null && courseObjects.indexOf(Object.keys(snapshot.val()))=== -1)
-        setCourseObjects({
-          ...snapshot.val()
-        });
-        console.log("hello",snapshot.val());
-    });
-   
-  }, []);
-
+  console.log(product);
   return (
     <TableBody>
-      {Object.keys(courseObjects).map(id => (
-        <Card className={clsx(classes.root, className)} {...rest}>
-          <CardContent>
-            <Typography
-              align="center"
-              color="textPrimary"
-              gutterBottom
-              variant="h4"
-            >
-              {courseObjects[id].courseTitle}
-            </Typography>
-            <Typography align="center" color="textPrimary" variant="body1">
-              {courseObjects[id].courseDescription}
-            </Typography>
-          </CardContent>
-          <Box flexGrow={1} />
-          <Divider />
-          <Box p={2}>
-            <Grid container justify="space-between" spacing={2}>
-              <Grid className={classes.statsItem} item>
-                <AccessTimeIcon className={classes.statsIcon} color="action" />
-                <Typography
-                  color="textSecondary"
-                  display="inline"
-                  variant="body2"
-                >
-                  {courseObjects[id].courseDuration ?? '1 month'}
-                </Typography>
-              </Grid>
-              <Grid className={classes.statsItem} item>
-                <AttachMoneyIcon className={classes.co} color="action" />
-                <Typography
-                  color="textSecondary"
-                  display="inline"
-                  variant="body2"
-                >
-                  {courseObjects[id].courseFee}
-                </Typography>
-              </Grid>
+      <Card className={clsx(classes.root, className)} {...rest}>
+        <CardContent>
+          <Typography
+            align="center"
+            color="textPrimary"
+            gutterBottom
+            variant="h4"
+          >
+            {product.courseTitle}
+          </Typography>
+          <Typography align="center" color="textPrimary" variant="body1">
+            {product.courseDescription}
+          </Typography>
+        </CardContent>
+        <Box flexGrow={1} />
+        <Divider />
+        <Box p={2}>
+          <Grid container justify="space-between" spacing={2}>
+            <Grid className={classes.statsItem} item>
+              <AccessTimeIcon className={classes.statsIcon} color="action" />
+              <Typography
+                color="textSecondary"
+                display="inline"
+                variant="body2"
+              >
+                {product.courseDuration ?? '1 month'}
+              </Typography>
             </Grid>
-          </Box>
-        </Card>
-      ))}
+            <Grid className={classes.statsItem} item>
+              <AttachMoneyIcon className={classes.co} color="action" />
+              <Typography
+                color="textSecondary"
+                display="inline"
+                variant="body2"
+              >
+                {product.courseFee}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Box>
+      </Card>
     </TableBody>
   );
 };
--- src/views/product/ProductListView/index.js
+++ src/views/product/ProductListView/index.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Container, Grid, makeStyles } from '@material-ui/core';
 import { Pagination } from '@material-ui/lab';
 import Page from 'src/components/Page';
 import Toolbar from './Toolbar';
 import ProductCard from './ProductCard';
 import data from './data';
+import firebaseDb from '../../../firebase';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -20,19 +21,26 @@ const useStyles = makeStyles(theme => ({
 
 const ProductList = () => {
   const classes = useStyles();
-  const [products] = useState(data);
-
+  var [courseObjects, setCourseObjects] = useState({});
+  useEffect(() => {
+    firebaseDb.child('coursedetails').on('value', snapshot => {
+      if (snapshot.val() != null)
+        setCourseObjects({
+          ...snapshot.val()
+        });
+    });
+  }, []);
   return (
     <Page className={classes.root} title="Products">
       <Container maxWidth={false}>
         <Toolbar />
         <Box mt={3}>
           <Grid container spacing={3}>
-            {products.map(product => (
+            {Object.keys(courseObjects).map(product => (
               <Grid item key={product.id} lg={4} md={6} xs={12}>
                 <ProductCard
                   className={classes.productCard}
-                  product={product}
+                  product={courseObjects[product]}
                 />
               </Grid>
             ))}
